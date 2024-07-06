c496ca729831e770ac26322ea66996d1158dd63e
Viktors Boroviks <vboroviks@inbox.lv>
2024-07-06 23:53:14 +0300

    TODO updated

--- include/tante.hpp
+++ include/tante.hpp
@@ -1,29 +1,133 @@
 namespace tante {
 
-// neuron
-// - sigma_func
-// - cache
-// - cache_updated
-// - inputs (why do i need those?)
-// - outputs
-//   - must be recalculated if cache_updated = true
-//   - link neurons or connections?
-//     - connections seem more natural
+// settings
+// - perturbation_class_weights
+//   - set to 0 to disable
+// - perturbation weights
+//   - weights for selecting different activation functions
+//     (setting particular activation f can become separate perturbation)
+// - max_n_neurons
 
-// connection (better name?)
-// - w_coef
+// neuron
+// - activation_function
+// - bias
+// - signal_cache
+// - signal_cache_updated (find a better name)
+// - inputs (why do i need to know those?)
+// - outputs
+//   - must be recalculated if signal_cache_updated = true
+// - inputs_allowed
+// - outputs_allowed
+// - unused
+//   - needed to be able to preserve history or grow paths that do not provide
+//     value before finished
+//   - mark unused
+//     - if no outputs
+//     - or if no inputs
+//     - or if all outputs are unused
+//     - or if no inputs
+//     - or if all inputs are unused
+//     - and still has at least 1 path connecting to the main graph
+//   - do not recalculate
+//   - return signal 0
+//   - connections not impacted
+
+// connection
+// - in_neuron
+// - out_neuron
+// - weight
 
 // network
 // - inputs
-// - output
+// - outputs
 // - interconnected neurons
 //   - neurons?
 //   - connections?
 //   - both?
-// - perturbate (change class)
+// - perturbate (perturbation classes)
 //   - add neuron
 //   - remove neuron
+//   - remove unused neuron
+//   - change neuron activation function
+//   - change neuron activation function parameters (bias)
 //   - add connection
 //   - remove connection
-//   - change connection coef
+//   - change connection weight
+// - limitations
+// - run
+
+// topology
+// - dag
+// - no cyclic paths
+// - no parallel paths of len 1
+// - no separate graphs
+// - every node except in/out
+//   - >=1 in
+//   - >=1 out
+// - in/out
+//   - in
+//     - only signal, no acceptance_function
+//   - out
+//     - only 1 input allowed (no sum of inputs)
+//     - no acceptance function
+
+// algorithms
+// - init new net
+//   - make random perturbations until first signal is passed from every output
+//   - a working net is needed to be able to perform its evaluations
+//     after every perturbation later
+// - restore net operability
+//   - not implemented
+//   - overcomplication for the first iteration
+// - add new node
+//   - propose graph with a new node
+//   - check no cycles
+//   - accept or repeat
+// - remove node
+//   - remove all related connections
+// - remove unused node
+//   - not implemented
+//   - overcomplication for the first iteration
+// - add new connection
+// - remove connection
+// - marking unused
+//   - not implemented
+//   - overcomplication for the first iteration
+// - evaluating net
+//   - calculate dependency tree from output
+//   - traverse dependency tree
+//   - save cache for every node
+// - saving cache
+//   - after calculation save result
+// - updating signal cache
+//   - on adding node
+//     - mark new node as modified
+//     - evaluate net
+//       - calculdate dependency tree
+//       - mark all out nodes of modified nodes as modified recursively
+//       - traverse dependency tree and calculate the result
+//   - on removing node
+//     - mark out nodes of the removed node as "modified"
+//     - mark all out nodes of modified nodes as modified recursively
+//     - evaluate net
+//   - on modifying node
+//     - mark node as "modified"
+//     - mark all out nodes of modified nodes as modified recursively
+//     - evaluate net
+//   - on adding connection
+//     - mark out node as "modified"
+//     - mark all out nodes of modified nodes as modified recursively
+//     - evaluate net
+//   - on removing connection
+//     - mark out node as "modified"
+//     - mark all out nodes of modified nodes as modified recursively
+//     - evaluate net
+//   - on modifying connection
+//     - mark out node as "modified"
+//     - mark all out nodes of modified nodes as modified recursively
+//     - evaluate net
+
+// classes
+// - ?
+
 }
